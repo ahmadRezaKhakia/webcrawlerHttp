cf42941c9c9854a6ea7000672d2566eb5cedaeb5
AhmadReza Khakia <khakia7961@gmail.com>
2023-06-27 09:55:37 +0330

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,35 @@
+const { JSDOM } = require('jsdom');
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = [];
+    const dom = new JSDOM(htmlBody);
+    const linkElements = dom.window.document.querySelectorAll('a');
+
+    for (const linkElement of linkElements) {
+        if (linkElement.href.slice(0, 1) === '/') {
+            //relative
+            try {
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(urlObj.href);
+            } catch (err) {
+                console.log(`Error with relative url ${err.message}`);
+            }
+        } else {
+            //absolute
+            try {
+                const urlObj = new URL(linkElement.href)
+                urls.push(urlObj.href);
+            } catch (err) {
+                console.log(`Error with absolute url ${err.message}`);
+            }
+        }
+
+    }
+
+    return urls;
+}
+
+
 function normalizeURl(urlString) {
     const urlObj = new URL(urlString);
     const hostpath = `${urlObj.hostname}${urlObj.pathname}`;
@@ -9,5 +41,6 @@ function normalizeURl(urlString) {
 }
 
 module.exports = {
-    normalizeURl
+    normalizeURl,
+    getURLsFromHTML
 }
